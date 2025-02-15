7dd8caaafc53825fb352c3e17c46fe21cde84257
Oliver Bilbie <68697462+Oliver-Bilbie@users.noreply.github.com>
2025-02-15 14:54:01 +0000

    Defer js until loaded

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,34 +3,36 @@ import { onResize, onScroll } from "./events.js";
 import { generateStars } from "./utils.js";
 import { WelcomeState } from "./welcomeState.js";
 
-const perspective = 800;
-const rotateX = 45;
+window.addEventListener("load", () => {
+  const perspective = 800;
+  const rotateX = 45;
 
-let clientSize = getClientSize();
-let contentHeight = getContentHeight();
-onResize(clientSize, contentHeight);
-
-const stars = generateStars(clientSize.width, contentHeight);
-
-const welcomeState = new WelcomeState();
-fadeInWelcomeText(welcomeState);
-
-document
-  .querySelector(".star-container")
-  .addEventListener("scroll", (event) => {
-    onScroll(
-      event.target.scrollTop,
-      rotateX,
-      perspective,
-      clientSize,
-      contentHeight,
-      stars,
-      welcomeState
-    );
-  });
-
-window.addEventListener("resize", () => {
-  clientSize = getClientSize();
-  contentHeight = getContentHeight();
+  let clientSize = getClientSize();
+  let contentHeight = getContentHeight();
   onResize(clientSize, contentHeight);
+
+  const stars = generateStars(clientSize.width, contentHeight);
+
+  const welcomeState = new WelcomeState();
+  fadeInWelcomeText(welcomeState);
+
+  document
+    .querySelector(".star-container")
+    .addEventListener("scroll", (event) => {
+      onScroll(
+        event.target.scrollTop,
+        rotateX,
+        perspective,
+        clientSize,
+        contentHeight,
+        stars,
+        welcomeState,
+      );
+    });
+
+  window.addEventListener("resize", () => {
+    clientSize = getClientSize();
+    contentHeight = getContentHeight();
+    onResize(clientSize, contentHeight);
+  });
 });
